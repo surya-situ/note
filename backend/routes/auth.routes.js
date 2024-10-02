02450ecba9999de1b3a7d553ac46597528dc4c95
surya-situ <suryasocial55@gmail.com>
2024-10-02 18:56:01 +0530

    feat: added verifyotp, requestopt and sigin along with delete user route

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { requestOtp, signin, verifyCreateUser } from "../controllers/auth.controller.js";
+import { deleteUser, requestOtp, signin, verifyCreateUser } from "../controllers/auth.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const authRouter = express.Router();
 
 authRouter.post('/signup', requestOtp);
 authRouter.post('/verify-otp', verifyCreateUser);
 authRouter.post('/signin', signin);
+authRouter.delete('/delete-user', authMiddleware, deleteUser);
 
 
 export default authRouter;
